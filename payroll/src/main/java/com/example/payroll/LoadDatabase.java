e0fbf6aec2151c5d05e046daad82735d8b42e611
Francis-Aasil <faasilg23@gmail.com>
2026-03-30 17:03:07 +0530

    Implemented Role management system and Navbar

--- payroll/src/main/java/com/example/payroll/LoadDatabase.java
+++ payroll/src/main/java/com/example/payroll/LoadDatabase.java
@@ -1,16 +1,15 @@
 package com.example.payroll;
 
-import com.example.payroll.role.Role;               
-import com.example.payroll.role.RoleRepository;     
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import com.example.payroll.role.Role;
+import com.example.payroll.role.RoleRepository;
 @Configuration
-class LoadDatabase {
+public class LoadDatabase {
 
     private static final Logger log = LoggerFactory.getLogger(LoadDatabase.class);
 
